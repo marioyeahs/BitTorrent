5329d66fa4c96dcd59301ad6d6d84dedef808045
marioyeahs <marioyeahs@gmail.com>
2023-01-04 00:52:15 -0600

    add: save file pieces on disk

--- client.py
+++ client.py
@@ -8,12 +8,19 @@ async def download(torrent_file):
     # Get peers list from tracker in ".torrent" file 
     peers_addresses = await get_peers(torrent_file)
 
+    # Queue for storing downloaded file pieces
+    file_pieces_queue = asyncio.Queue()
+
+    # Object to coordinate writing file to disk
+    file_saver = FileSaver(file_pieces_queue)
+
     # Object to track peers communication/state
-    peers = [Peer(addr) for addr in peers_addresses]
+    peers = [Peer(addr, file_pieces_queue) for addr in peers_addresses]
 
     # Wait for all download coroutines to finish
     await asyncio.gather(
-            *[peer_download() for peer in peers] # Producer
+            *([peer_download() for peer in peers] +  # Producer
+            [file_saver.start()])                    # Consumer
         ) 
 
 if __name__ == '__main__':
